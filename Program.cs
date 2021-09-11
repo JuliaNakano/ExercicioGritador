8ea3d2ce0ada2f6356b0796563c5349a1813c5a8
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-11 14:07:43 -0300

    Solução do exercício Gritador

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExercicioGritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frasedigitada;
+            Console.Write("Digite uma frase: ");
+            frasedigitada= Console.ReadLine(); 
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine($"{frasedigitada}".ToUpper());
+            Console.ResetColor();
+
+
+        }
+    }
+}
